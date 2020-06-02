ea70ad2e0b79b2cdab6f9ac6656b1a2d7c679c11
Yash Parmar <yashparmar15700@gmail.com>
2020-06-02 16:42:25 +0530

    Some Modifications

--- FoodWagon/settings.py
+++ FoodWagon/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Foodwagon',
         'USER': 'postgres',
-        'PASSWORD': 'P@ssw0rd',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
