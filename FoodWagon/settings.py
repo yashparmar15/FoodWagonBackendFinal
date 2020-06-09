bee68d4f268395215359291677a072e09dd2b764
Ayan <ayan.adhya.18003@iitgoa.ac.in>
2020-06-09 22:23:29 +0530

    added the 404 page and made other css changes

--- FoodWagon/settings.py
+++ FoodWagon/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Foodwagon',
         'USER': 'postgres',
-        'PASSWORD': '1234ayan',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
